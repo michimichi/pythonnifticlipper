0c120be15f5764a4c4511ef640e8c96e8326c40d
Michael Ingrisch <michael.ingrisch@med.lmu.de>
2014-11-19 21:57:27 +0100

    appears to run...

--- python_clip_images.py
+++ python_clip_images.py
@@ -4,9 +4,7 @@ Load a nifti image, remove nans, clip the values between zero and 1000, write it
 Required for the peak enhancement images 
 
 Todo: 
-
-    * check argparse: how do we get a filename from the commandline
-    * wcall it with a zsh loop
+    * call it with a zsh loop
 """
 import os
 import argparse
@@ -22,25 +20,28 @@ def python_clip_images(filename, vmin=0, vmax=2000, outfilename=None):
     # needs generalization! the infile could be a different fileformat
     outimage = nib.Nifti1Image(ndata, image.get_affine())
     if not outfilename:
-         
-         outfilename = filename.replace('.nii', '_clip.nii')
+        outfilename = filename.replace('.nii', '_clip.nii')
     outimage.to_filename(outfilename)
 
-if __name__=="__main__":
-   parser = argparse.ArgumentParser()
-   parser.add_argument("infile", help="Input file to be clipped, as nifti)
-   parser.add_argument("--outfile", "-o", help="Output filename (optional). Default is infile_clip.nii")
-   parser.add_argument("--vmin", type=float, help="Lower boundary of the clipped image")
-   parser.add_argument("--vmax", type=float, help="Upper boundary of the clipped image")
-   
-   args = parser.parse_args()
-   vmin, vmax=(0,2000)
-   if args.vmin:
-      vmin=args.vmin
-   if args.vmax:
-      vmax=args.vmax
-   
-   python_clip_images(args.infile, vmin=vmin, vmax=vmax, outfilename=outfilename)
-   
-   # todo: some testing is required
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("infile", help="Input file to be clipped, as nifti")
+    #parser.add_argument(
+    #    "--outfile", "-o", help="Output filename (optional). Default is infile_clip.nii")
+    parser.add_argument(
+        "--vmin", type=float, help="Lower boundary of the clipped image")
+    parser.add_argument(
+        "--vmax", type=float, help="Upper boundary of the clipped image")
 
+    args = parser.parse_args()
+    vmin, vmax = (0, 2000)
+    if args.vmin:
+        vmin = args.vmin
+    if args.vmax:
+        vmax = args.vmax
+
+    python_clip_images(
+        #args.infile, vmin=vmin, vmax=vmax, outfilename=outfilename)
+        args.infile, vmin=vmin, vmax=vmax)
+
+    # todo: some testing is required
